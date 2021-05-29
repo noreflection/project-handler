e3c82da59810a682f51c64cba50c6f528b97fb5f
noreflection <a191a191a@gmail.com>
2021-05-29 20:23:53 +0300

    initial

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+extern crate git2;
+
+use git2::Repository;
+
+fn main() {
+    let repo_root = std::env::args().nth(1).unwrap_or(".".to_string());
+    let repo = Repository::open(repo_root.as_str()).expect("Couldn't open repository");
+    println!("{} state={:?}", repo.path().display(), repo.state());
+}
